e40df3c3cc19380f63a1b30d96cd6273ae74883e
Florian Müller <fmui@apache.org>
2017-03-10 20:19:14 +0000

    CMIS-1018: fixed number formatting
    git-svn-id: https://svn.apache.org/repos/asf/chemistry/portcmis/trunk@1786433 13f79535-47bb-0310-9956-ffa450edef68

--- PortCMIS/binding/Url.cs
+++ PortCMIS/binding/Url.cs
@@ -162,7 +162,7 @@ namespace PortCMIS.Binding
             }
             else if (value is BigInteger)
             {
-                return ((BigInteger)value).ToString("#", CultureInfo.InvariantCulture);
+                return ((BigInteger)value).ToString("0", CultureInfo.InvariantCulture);
             }
 
             return value.ToString();
